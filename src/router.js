77be7f9a18f6de0fc25b6b041d009ca227e8cbe4
Joyce Cheung <442394485@qq.com>
2019-11-28 12:40:33 +0800

    Add-Page title

--- src/router.js
+++ src/router.js
@@ -1,59 +1,101 @@
 const routes = [
   {
     path: '/',
-    component: () => import('./views/Dashboard.vue')
+    component: () => import('./views/Dashboard.vue'),
+    meta: {
+      title: 'My Dashboard'
+    }
   },
   {
     path: '/myartworks',
-    component: () => import('./views/Myartworks.vue')
+    component: () => import('./views/Myartworks.vue'),
+    meta: {
+      title: 'My Artworks'
+    }
   },
   {
     path: '/artwork/:id',
-    component: () => import('./views/Artwork.vue')
+    component: () => import('./views/Artwork.vue'),
+    meta: {
+      title: 'My Artworks'
+    }
   },
   {
     path: '/about',
-    component: () => import('./views/About.vue')
+    component: () => import('./views/About.vue'),
+    meta: {
+      title: 'My Artworks'
+    }
   },
   {
     path: '/myprojects',
-    component: () => import('./views/Myprojects.vue')
+    component: () => import('./views/Myprojects.vue'),
+    meta: {
+      title: 'My Projects'
+    }
   },
   {
     path: '/project/:id',
-    component: () => import('./views/Project.vue')
+    component: () => import('./views/Project.vue'),
+    meta: {
+      title: 'My Projects'
+    }
   },
   {
     path: '/roomtype/:id/',
-    component: () => import('./views/Roomtype.vue')
+    component: () => import('./views/Roomtype.vue'),
+    meta: {
+      title: 'My Roomtypes'
+    }
   },
   {
     path: '/addproduct-keypad/:id/',
-    component: () => import('./views/Addproduct-EA.vue')
+    component: () => import('./views/Addproduct-EA.vue'),
+    meta: {
+      title: 'Add a New Product'
+    }
   },
   {
     path: '/addproduct-sockets/:id/',
-    component: () => import('./views/Addproduct-SK.vue')
+    component: () => import('./views/Addproduct-SK.vue'),
+    meta: {
+      title: 'Add a New Product'
+    }
   },
   {
     path: '/dashboard/',
-    component: () => import('./views/Dashboard.vue')
+    component: () => import('./views/Dashboard.vue'),
+    meta: {
+      title: 'Dashboard'
+    }
   },
   {
     path: '/sales/',
-    component: () => import('./views/Dashboard-sales.vue')
+    component: () => import('./views/Dashboard-sales.vue'),
+    meta: {
+      title: 'Dashboard'
+    }
   },
   {
     path: '/collection-ab/',
-    component: () => import('./views/Collection-AB.vue')
+    component: () => import('./views/Collection-AB.vue'),
+    meta: {
+      title: 'Add a Collection'
+    }
   },
   {
     path: '/collection-ar/',
-    component: () => import('./views/Collection-AR.vue')
+    component: () => import('./views/Collection-AR.vue'),
+    meta: {
+      title: 'Add a Collection'
+    }
   },
   {
     path: '/alba/step',
-    component: () => import('./views/Collection-AB-step.vue')
+    component: () => import('./views/Collection-AB-step.vue'),
+    meta: {
+      title: 'Add a Collection'
+    }
   }
 ]
 
